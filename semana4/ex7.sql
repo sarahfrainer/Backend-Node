1729009780a604aeb91342ba6b2146e414445e8c
SarahFrainer <sbfrainer@gmail.com>
2024-06-08 15:01:16 -0300

    ex-7

---
+++ semana4/ex7.sql
@@ -0,0 +1,6 @@
+-- Função Agregada: Encontre a média de idade dos clientes que têm pedidos com valor total superior a 300,00.
+
+
+
+
+-- Subconsulta: Liste todos os produtos que foram pedidos mais de uma vez.
